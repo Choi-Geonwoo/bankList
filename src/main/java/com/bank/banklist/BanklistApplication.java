0a6d962f55d0e0252910d784bbf9af48c36d079c
ChoeGeonu1 <rjsdn911@@>
2023-06-21 21:07:13 +0900

    초기 설정

---
+++ src/main/java/com/bank/banklist/BanklistApplication.java
@@ -0,0 +1,13 @@
+package com.bank.banklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklistApplication.class, args);
+	}
+
+}
